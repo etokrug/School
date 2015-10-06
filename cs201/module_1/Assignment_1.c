09b41ce8da057765b7d7203b971bec37b1002df6
etokrug <etokrug@gmail.com>
2015-10-05 21:23:19 -0700

    Initial commit

---
+++ cs201/module_1/Assignment_1.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Assignment_1.c
+ *
+ *    Description:  This program converts decimals to binary.
+ *                      It takes input from a user (expecting a proper input
+ *                      with no error handling).
+ *
+ *        Version:  1.0
+ *        Created:  10/04/2015 09:11:15 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  William Brown
+ *   Organization:  PCC
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main() {
+    long int decimal, remainder, quotient;
+
+    int binaryNumber[100], i=1, j;
+
+
+
+
+}
