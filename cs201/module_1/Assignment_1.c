aad3c32b818d00fa54aec0f6721f318819d581f1
etokrug <etokrug@gmail.com>
2015-10-05 21:59:16 -0700

    Completion commit

--- cs201/module_1/Assignment_1.c
+++ cs201/module_1/Assignment_1.c
@@ -13,18 +13,58 @@
  *       Compiler:  gcc
  *
  *         Author:  William Brown
+ *          Class:  CS 201
+ *      Professor:  Ashis Chatterjee
  *   Organization:  PCC
  *
  * =====================================================================================
  */
-#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
-    long int decimal, remainder, quotient;
+    // Main set of declarations here
+    int decimal;
+    int remainder;
+    int quotient;
 
-    int binaryNumber[100], i=1, j;
+    // This is the array that will end up (I hope) holding the full binary number
+    // I set it to 32 to represent a 32 bit system - I think
+    int allNumbers[32];
 
+    // Main input here - still getting used to C vs C++/C#
+    printf("Enter Decimal Number: ");
+    scanf("%d", &decimal);
 
+    // This sets the quotient equal to the decimal number.
+    // This is done so that it can be later manipulated into the correct number.
+    quotient = decimal;
 
+    // This loop, although you stated pretty clearly what it should do
+    // gave me a lot of problems and I had to search for some additional
+    // online help. It's not that loops give me problems, rather this math
+    // is not something I'm used to on any level and I'm kind of two weeks
+    // behind because of my trip to Germany that we spoke about.
+    int i = 1;
+    while(quotient > 0){
+        allNumbers[i++] = quotient % 2;
+        quotient = quotient / 2;
+    }
 
+    // Standard output
+    printf("Output Binary Number: ");
+
+    // This was another loop that I had to get help with online
+    // I'm used to working with blocks or strings that print easily.
+    // Looking at this now it's clear to see that it's easy but it
+    // took me a while to arrive at this.
+    int j;
+    for(j = i - 1; j > 0; j--){
+        printf("%d", allNumbers[j]);
+    }
+
+    // Classically, I couldn't figure out why the output kept wrapping back onto
+    // itself. Solution, add a newline.
+    printf("\n");
+    
+    return 0;
 }
