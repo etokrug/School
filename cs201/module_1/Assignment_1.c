c021f5c9944e3d2ae808fea132cb0006d0c6f38c
etokrug <etokrug@gmail.com>
2015-10-19 23:18:16 -0700

    Altered for '32 bit' format

--- cs201/module_1/Assignment_1.c
+++ cs201/module_1/Assignment_1.c
@@ -29,33 +29,40 @@ int main() {
     static int false = 0;
     int negative = false;
     
-    // This is the array that will end up (I hope) holding the full binary number
-    // I set it to 31 to represent a 32 bit system - I think
-    int allNumbers[32];
-    
+    // Initialize the "32 bit" int arrays we'll be using to handle the conversions
+    int allNumbers[33];
+    int tempNumbers[33];
+    int numbersLen = sizeof(allNumbers)/sizeof(int);
+    int tempLen = 0;
 
-    // Main input here - still getting used to C vs C++/C#
+    // Main input here
     printf("Enter Decimal Number: ");
     scanf("%d", &decimal);
 
+    // If the number is negative switch it to positive for handling.
+    // Mark the negative bool as true.
     if (decimal < 0) {
         negative = true;
         decimal *= -1;
     }
     
+    // Initizalize the values of the "32 bit" array
+    int initializeInt = 0;
+    for (initializeInt; initializeInt < numbersLen; initializeInt++) {
+        if (negative) {
+            allNumbers[initializeInt] = 1;
+        }
+        else {
+            allNumbers[initializeInt] = 0;
+        }
+    }
     // This sets the quotient equal to the decimal number.
     // This is done so that it can be later manipulated into the correct number.
     remainder = decimal;
 
-    int i = 1;
+    int temp = 1;
     int remainderHolder = 0;
     while(remainder > 0){
-        if (i == 1) {
-            if (!negative) {
-            
-            }
-        }
-
         if (negative) {
            if (remainder % 2 == 0) {
                remainderHolder = 1;
@@ -63,22 +70,39 @@ int main() {
            else {
                remainderHolder = 0;
            }
-
         }
         else {
             remainderHolder = remainder % 2;
         }
-    
-        allNumbers[i++] = remainderHolder;
+        tempNumbers[temp++] = remainderHolder;
         remainder /= 2;
+        if (remainder == 0 && negative) {
+            tempNumbers[temp] = 1;
+        }
+        else { tempNumbers[temp] = 0; }
+    }
+
+    // If sign is negative add one to
+    tempLen = sizeof(tempNumbers)/sizeof(int);
+    
+    int stopper = 0;
+    while (temp > stopper) {
+        allNumbers[stopper] = tempNumbers[stopper];
+        stopper++;
     }
 
     // Standard output
     printf("Output Binary Number: ");
 
-    int j = i - 1;
-    for(j; j > 0; j--){
-        printf("%d", allNumbers[j]);
+    int space = 0;
+    int all = numbersLen - 1;
+    for(all; all > 0; all--){
+        if (space == 4) {
+            printf("%s", " ");
+            space = 0;
+        }
+        printf("%d", allNumbers[all]);
+        space++;
     }
 
     printf("\n");
