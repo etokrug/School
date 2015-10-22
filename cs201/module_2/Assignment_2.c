31be1790741e9ebaba326e4d5b9d6bafb9a17092
etokrug <etokrug@gmail.com>
2015-10-21 17:04:51 -0700

    First change

--- cs201/module_2/Assignment_2.c
+++ cs201/module_2/Assignment_2.c
@@ -1,15 +1,16 @@
 /*
  * =====================================================================================
  *
- *       Filename:  Assignment_1.c
+ *       Filename:  Assignment_2.c
  *
- *    Description:  This program converts decimals to binary.
- *                      It takes input from a user (expecting a proper input
- *                      with no error handling).
+ *    Description:  This program converts decimals to binary and hex.
+ *                  It takes input from a user (expecting a proper input
+ *                  with no error handling).
+ *                  It extracts bits from HEX values and shifts them.
  *
- *        Version:  1.1
- *        Created:  10/04/2015 09:11:15 AM
- *       Revision:  10/19/2015
+ *        Version:  1.0
+ *        Created:  10/21/2015
+ *       Revision:  NONE
  *       Compiler:  gcc
  *
  *         Author:  William Brown
@@ -24,26 +25,49 @@
 int main() {
     // Main set of declarations here
     int decimal;
-    int remainder;
-    int negative = 0;
     
-    // Initialize the "32 bit" int arrays we'll be using to handle the conversions
-    int allNumbers[33];
-    int tempNumbers[33];
-    int numbersLen = sizeof(allNumbers)/sizeof(int);
-
     // Main input here
     printf("Enter Decimal Number: ");
     scanf("%d", &decimal);
 
-    // If the number is negative switch it to positive for handling.
+    // TODO: Add in calls to your methods here
+
+    // Standard output
+    printf("Output Binary Number: ");
+
+    // Final binary print statement with a space added
+    // every four bits for clarity.
+    int space = 0;
+    int all = numbersLen - 1;
+    for(all; all > 0; all--){
+        if (space == 4) {
+            printf("%s", " ");
+            space = 0;
+        }
+        printf("%d", allNumbers[all]);
+        space++;
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+int[] convertDecToBinary(int converter) {
+    // Initialize the "32 bit" int arrays we'll be using to handle the conversions
+    int allNumbers[33];
+    int tempNumbers[33];
+    int numbersLen = sizeof(allNumbers)/sizeof(int);
+int remainder;
+    int negative = 0;
+    
+ // If the number is negative switch it to positive for handling.
     // Mark the negative bool as true.
-    if (decimal < 0) {
+    if (converter < 0) {
         negative = 1;
-        decimal *= -1;
+        converter *= -1;
     }
     
-    // Initizalize the values of the "32 bit" array
+// Initizalize the values of the "32 bit" array
     int initializeInt = 0;
     for (initializeInt; initializeInt < numbersLen; initializeInt++) {
         if (negative) {
@@ -53,11 +77,11 @@ int main() {
             allNumbers[initializeInt] = 0;
         }
     }
-    // This sets the quotient equal to the decimal number.
+// This sets the quotient equal to the decimal number.
     // This is done so that it can be later manipulated into the correct number.
     remainder = decimal;
 
-    // Main body of the program here.
+// Main body of the program here.
     // Loads the temporary array with the final values if positive.
     // Loads the temporary arry with the inverted values if negative.
     int temp = 1;
@@ -117,22 +141,11 @@ int main() {
         }
     }
 
-    // Standard output
-    printf("Output Binary Number: ");
 
-    // Final binary print statement with a space added
-    // every four bits for clarity.
-    int space = 0;
-    int all = numbersLen - 1;
-    for(all; all > 0; all--){
-        if (space == 4) {
-            printf("%s", " ");
-            space = 0;
-        }
-        printf("%d", allNumbers[all]);
-        space++;
-    }
-    printf("\n");
-    
-    return 0;
+
+}
+
+char[] convertDecToHex(int converter) {
+
+
 }
