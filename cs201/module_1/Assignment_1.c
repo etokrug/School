86a9226866fdc02cb3c19e598eed66fcf3539d1e
etokrug <etokrug@gmail.com>
2015-10-22 10:00:10 -0700

    Refactoring for A1

--- cs201/module_1/Assignment_1.c
+++ cs201/module_1/Assignment_1.c
@@ -7,9 +7,9 @@
  *                      It takes input from a user (expecting a proper input
  *                      with no error handling).
  *
- *        Version:  1.1
+ *        Version:  1.2
  *        Created:  10/04/2015 09:11:15 AM
- *       Revision:  10/19/2015
+ *       Revision:  10/21/2015
  *       Compiler:  gcc
  *
  *         Author:  William Brown
@@ -19,120 +19,121 @@
  *
  * =====================================================================================
  */
+#
 #include <stdio.h>
+int* invertBits(int* bitsToFlip, int arraySize);
+int* addOneBitToBitArray(int* bitsToAddTo, int arraySize);
+void printBinaryArray(int* binaryArray, int arraySize);
+void printHexArray(char* hexArray);
+int* convertDecToBinary(int converter, int* allNumbers, int arraySize);
 
 int main() {
     // Main set of declarations here
     int decimal;
-    int remainder;
-    int negative = 0;
-    
-    // Initialize the "32 bit" int arrays we'll be using to handle the conversions
-    int allNumbers[33];
-    int tempNumbers[33];
-    int numbersLen = sizeof(allNumbers)/sizeof(int);
+    int arraySize = 32;
+    int numberArray[arraySize - 1];
 
     // Main input here
     printf("Enter Decimal Number: ");
     scanf("%d", &decimal);
 
+    convertDecToBinary(decimal, numberArray, arraySize);
+
+    // Standard output
+    printf("Output Binary Number: ");
+    printBinaryArray(numberArray, arraySize);
+    printf("\n");
+
+    return 0;
+}
+
+int* convertDecToBinary(int converter, int* allNumbers, int arraySize) {
+    // Initialize the n-bit int arrays we'll be using to handle the conversions
+    int numbersLen = arraySize - 1;
+    int remainder;
+    int negative = 0;
+    
     // If the number is negative switch it to positive for handling.
     // Mark the negative bool as true.
-    if (decimal < 0) {
+    if (converter < 0) {
         negative = 1;
-        decimal *= -1;
+        converter *= -1;
     }
     
-    // Initizalize the values of the "32 bit" array
+    // Initizalize the values of the n-bit array
     int initializeInt = 0;
     for (initializeInt; initializeInt < numbersLen; initializeInt++) {
-        if (negative) {
-            allNumbers[initializeInt] = 1;
-        }
-        else {
-            allNumbers[initializeInt] = 0;
-        }
+        allNumbers[initializeInt] = 0;
     }
     // This sets the quotient equal to the decimal number.
     // This is done so that it can be later manipulated into the correct number.
-    remainder = decimal;
+    remainder = converter;
 
     // Main body of the program here.
     // Loads the temporary array with the final values if positive.
     // Loads the temporary arry with the inverted values if negative.
-    int temp = 1;
-    int remainderHolder = 0;
+    int temp = numbersLen;
     while(remainder > 0){
-        if (negative) {
-           if (remainder % 2 == 0) {
-               remainderHolder = 1;
-           }
-           else {
-               remainderHolder = 0;
-           }
+        allNumbers[temp--] = remainder % 2;
+        remainder /= 2;
+    }
+
+    if (negative) {
+        invertBits(allNumbers, arraySize);
+        addOneBitToBitArray(allNumbers, arraySize);
+    }
+
+    return allNumbers;
+}
+
+int* invertBits(int* bitsToFlip, int arraySize) {
+    //int bitLen = arraySize - 1;
+
+    int i = 0;
+    for (i; i < arraySize; i++) {
+        if (bitsToFlip[i] == 0) {
+            bitsToFlip[i] = 1;
         }
         else {
-            remainderHolder = remainder % 2;
+            bitsToFlip[i] = 0;
         }
-        tempNumbers[temp++] = remainderHolder;
-        remainder /= 2;
-        if (remainder == 0 && negative) {
-            tempNumbers[temp] = 1;
-        }
-        else { tempNumbers[temp] = 0; }
     }
+    
+    return bitsToFlip;
+}
 
-    // Transfer the information for our temp array to the main one for printing
-    int stopper = 0;
-    while (temp > stopper) {
-        allNumbers[stopper] = tempNumbers[stopper];
-        stopper++;
-    }
-
-    // Add one to the the final array if it is negative so that it can
-    // be a complete Two's Complement.
-    stopper = 0;
+int* addOneBitToBitArray(int* bitsToAddTo, int arraySize) {
     int breakBit = 1;
-    int carryBit = 0;
-    if (negative) {
-        while (stopper < numbersLen) {
-            if (allNumbers[stopper] == 0) {
-                if (breakBit || carryBit) {
-                    allNumbers[stopper] = 1;
-                    breakBit = 0;
-                    carryBit = 0;
-                }
-                else {
-                    breakBit = 0;
-                    carryBit = 0;
-                }
+    int stopper = arraySize - 1;
+    while (stopper >= 0) {
+        if (bitsToAddTo[stopper] == 0) {
+            if (breakBit) {
+                bitsToAddTo[stopper] = 1;
+                breakBit = 0;
             }
-            else {
-                if (carryBit == 0) {
-                    allNumbers[stopper] = 0;
-                }
-            }
-            if (!breakBit) { break; }
-            stopper++;
         }
+        else {
+            bitsToAddTo[stopper] = 0;
+        }
+        if (!breakBit) { break; }
+        stopper--;
     }
+    return bitsToAddTo;
+}
 
-    // Standard output
-    printf("Output Binary Number: ");
 
-    // Final binary print statement with a space added
+void printBinaryArray(int* binaryArray, int arraySize) {
+    if (!binaryArray) { printf("\nThe binary array passed was empty!\n"); }
+    
     // every four bits for clarity.
     int space = 0;
-    int all = numbersLen - 1;
-    for(all; all > 0; all--){
+    int i = 0;
+    for(i; i < arraySize; i++){
         if (space == 4) {
-            printf("%s", " ");
+            printf(" ");
             space = 0;
         }
-        printf("%d", allNumbers[all]);
+        printf("%d", binaryArray[i]);
         space++;
     }
-    printf("\n");
-    
-    return 0;
 }
