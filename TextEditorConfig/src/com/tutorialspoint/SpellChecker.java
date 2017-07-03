bfc848fe08b89808805e8b2dbcf32ad29e1a7b62
etokrug <etokrug@gmail.com>
2017-07-02 20:49:27 -0700

    Commit time

---
+++ TextEditorConfig/src/com/tutorialspoint/SpellChecker.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint;
+
+/**
+ * Created by William on 7/2/2017.
+ */
+public class SpellChecker {
+    public SpellChecker () {
+        System.out.println("Inside SpellChecker Constructor");
+
+    }
+    public void checkSpelling(){
+        System.out.println("Inside checkSpelling");
+    }
+}
--- oca/src/Zoo.java
+++ oca/src/Zoo.java
@@ -1,10 +1,32 @@
 /**
   Created by William on 3/21/2017.
  */
+
+
 public class Zoo {
     public static void main(String[] args) {
         System.out.println("Welcome");
         System.out.println(args[0]);
         System.out.println(args[1]);
+        String x = null;
+        System.out.println(x);
+        boolean y = false;
+        System.out.println(y);
+        double amt = 0xE;
+        System.out.println(amt);
+        System.out.println("0b110000111000 = " + 0b110000111000);
+        double x2 = 39.21;
+        System.out.println("double x2 = " + x2);
+        float y2 = 2.1f;
+        System.out.println("float y2 = " + y2);
+        System.out.println("y2 + x2 = " + (y2 + x2));
+        int z = 1;
+        int m = 1;
+        int p = 0;
+        while (p < 10) {
+            p = m < 10 ? m++ : z++;
+            System.out.println("m = " + m + " | z = " + z);
+        }
+
     }
 }
